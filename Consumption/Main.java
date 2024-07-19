6fb3bf5a535c0a45629f01eec3dc56dcffeb0e7a
EngelRyan <ryaneengel@gmail.com>
2024-07-19 10:52:33 -0300

    Challenge Cobsumption

---
+++ Consumption/Main.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner input = new Scanner(System.in);
+
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        
+        double average = a/b;
+
+        System.out.printf("%.3f km/l",average);
+        System.out.println();
+
+        input.close();
+    }
+}
+
