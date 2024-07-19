0655787a7b028abae3ffe97d154130db72fe7bce
EngelRyan <ryaneengel@gmail.com>
2024-07-19 09:46:02 -0300

    Challenge Distance

---
+++ Distance/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner input = new Scanner(System.in);
+
+        int distance = input.nextInt();
+
+        int howLong =  distance*2;
+
+        System.out.printf("%d minutos",howLong);
+        System.out.println();
+        
+        input.close();
+
+    }
+}
