057bdab16517fd12ea5ea8d1520eb2cb1729435e
EngelRyan <ryaneengel@gmail.com>
2024-07-23 08:56:18 -0300

    Selection

---
+++ Selection Test 1/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int d = input.nextInt();
+
+        String condition = (b>c && d>a && c+d>a+b && c>0 && d>0 && a%2==0) ? "Valores aceitos" : "Valores nao aceitos";
+
+        System.out.print(condition);
+        System.out.println();
+
+        input.close();
+    }
+
+}
