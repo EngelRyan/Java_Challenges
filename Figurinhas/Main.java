68fb06c998721a6c3e190e9b2a1b74b224e07c13
EngelRyan <ryaneengel@gmail.com>
2024-07-24 23:07:11 -0300

    Figures

---
+++ Figurinhas/Main.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+       Scanner input = new Scanner(System.in);
+
+       int n = input.nextInt();
+       ArrayList<Integer> values = new ArrayList<>();
+ 
+        while (n --> 0) {
+                int f1 = input.nextInt();
+                int f2 = input.nextInt();
+                values.add(gcd(f1, f2)); 
+                
+        }
+        for(int value : values){
+            System.out.println(value);
+        }
+
+       input.close();
+    }
+    public static int gcd(int f1,int f2){
+        int greatestDiv = 1;
+
+        while(f2!=0){
+            int temp = f2;
+            f2 = f1%f2;
+            f1 = temp;
+        }
+        return f1;
+    }
+ 
+}
