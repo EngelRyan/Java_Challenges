fee733557bae98edc0310925b28ac33a03125039
EngelRyan <ryaneengel@gmail.com>
2024-07-26 12:08:28 -0300

    Friends

--- MATHEMATICS/Friends/Main.java
+++ MATHEMATICS/Friends/Main.java
@@ -1,5 +1,3 @@
-package MATHEMATICS.Friends;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,11 +9,11 @@ public class Main {
 
         int t = input.nextInt();
         ArrayList<Integer> values = new ArrayList<>();
-
+        
         while(t --> 0){
             int m = input.nextInt();
             int n = input.nextInt();
-            System.out.println("TESTE");
+            values.add(pay(m,n));
         }
         
         for(int value : values){
@@ -23,5 +21,16 @@ public class Main {
         }
         input.close();
         
+    }
+    private static int pay(int m, int n){
+        if(m==0){
+            return n+1;
+        }
+        else if(n==0){
+            return pay(m-1,1); 
+        }
+        else{
+            return pay(m-1, pay(m, n-1));
+        }
     }
 }
