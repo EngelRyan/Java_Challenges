6612aed004891565d00cb8280cbb6728201055cd
EngelRyan <ryaneengel@gmail.com>
2024-07-27 14:38:05 -0300

    Update

--- MATHEMATICS/Friends/Main.java
+++ MATHEMATICS/Friends/Main.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Main {
+
+    private static int[][] memo = new int[5][201]; 
+
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
@@ -23,14 +25,22 @@ public class Main {
         
     }
     private static int pay(int m, int n){
+        String key = m + "," + n;
+
+
+        int result;
+
         if(m==0){
-            return n+1;
+            result = n+1;
         }
         else if(n==0){
-            return pay(m-1,1); 
+            result = pay(m-1,1); 
         }
         else{
-            return pay(m-1, pay(m, n-1));
+            result = pay(m-1, pay(m, n-1));
         }
+
+        
+        return result;
     }
 }
