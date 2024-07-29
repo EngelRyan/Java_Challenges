0d8283fcec6d67830c35fe338e04ac7f9e52adb0
EngelRyan <ryaneengel@gmail.com>
2024-07-29 12:06:56 -0300

    Solved

--- MATHEMATICS/Digits_Count/Main.java
+++ MATHEMATICS/Digits_Count/Main.java
@@ -1,33 +1,32 @@
-package MATHEMATICS.Digits_Count;
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
 
-        int a = input.nextInt();
-        int b = input.nextInt();
-    
-        String[] numberToStrings = new String[b-a+1];
-        String[] numbers = {"0","1","2","3","4","5","6","7","8","9"};
+        while (true) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            
+            if (a == 0 && b == 0) {
+                break;
+            }
 
-        for(int i = a, n = 0; i<=b; i++,n++){
-            numberToStrings[n] = String.valueOf(i);
-        }
-        
-        for(String number : numbers){
-            int q = 0;
-            for(String numString : numberToStrings){
-                if (numString.contains(number)) {
-                    q += 1;
+            int[] digitCounts = new int[10]; 
+
+            for (int i = a; i <= b; i++) {
+                String number = String.valueOf(i);
+                for (char digit : number.toCharArray()) {
+                    digitCounts[digit - '0']++;
                 }
             }
-            System.out.print(q);
+
+            for (int count : digitCounts) {
+                System.out.print(count + " ");
+            }
+            System.out.println();
         }
         input.close();
     }
 }
-
