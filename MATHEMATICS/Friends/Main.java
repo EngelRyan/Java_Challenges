53908e2d221ad15dc97913762af244f620b1b09c
EngelRyan <ryaneengel@gmail.com>
2024-07-29 11:16:16 -0300

    Update

--- MATHEMATICS/Friends/Main.java
+++ MATHEMATICS/Friends/Main.java
@@ -2,31 +2,29 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
 
         int t = input.nextInt();
-        int[] results = new int[t];
 
-        for (int i = 0; i < t; i++) {
+        while (t-- > 0) {
             int m = input.nextInt();
             int n = input.nextInt();
-            results[i] = pay(m, n);
+            System.out.println(pay(m, n));
         }
 
-        for (int result : results) {
-            System.out.println(result);
-        }
         input.close();
     }
 
     private static int pay(int m, int n) {
+        int result;
         if (m == 0) {
-            return n + 1;
+            result = n + 1;
         } else if (n == 0) {
-            return pay(m - 1, 1);
+            result = pay(m - 1, 1);
         } else {
-            return  pay(m - 1, pay(m, n - 1));
+            result = pay(m - 1, pay(m, n - 1));
         }
+        return result;
     }
 }
