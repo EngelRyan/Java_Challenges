14c4d1dba63c8f395ab6ad89d1af4d85fd0af2c6
EngelRyan <ryaneengel@gmail.com>
2024-07-28 21:59:45 -0300

    Update

--- MATHEMATICS/Friends/Main.java
+++ MATHEMATICS/Friends/Main.java
@@ -23,7 +23,7 @@ public class Main {
             System.out.println(value);
         }
         input.close();
-        int temp = 1;
+        int temp = 2;
     }
     private static int pay(int m, int n){
         if (memo[m][n] != 0) {
