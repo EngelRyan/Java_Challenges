eef66b16eb8c3527e46b7ba80ae452bb90a78c2d
EngelRyan <ryaneengel@gmail.com>
2024-07-29 11:26:49 -0300

    New Challenge

---
+++ MATHEMATICS/Digits_Count/Main.java
@@ -0,0 +1,15 @@
+package MATHEMATICS.Digits_Count;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+       
+
+        input.close();
+    }
+}
+
