4b607800020ede5c68ec39e8da5738b811e7a630
EngelRyan <ryaneengel@gmail.com>
2024-08-01 11:26:05 -0300

    New

--- BEGINNER/Taxes/Main.java
+++ BEGINNER/Taxes/Main.java
@@ -5,6 +5,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
 
+        double salary = input.nextDouble();
+
+        double taxes = 0;
+
+        if (salary>4500) {
+            taxes = ((salary-4500)*28)/100;
+            System.out.println(taxes);
+        }
        
         input.close();
     }
