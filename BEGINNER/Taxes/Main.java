f57c7561f799edadf91c4a9cbfecbf3a78f7e501
EngelRyan <ryaneengel@gmail.com>
2024-08-01 11:13:50 -0300

    New

---
+++ BEGINNER/Taxes/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+       
+        input.close();
+    }
+}
