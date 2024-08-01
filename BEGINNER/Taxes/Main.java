823cb67df46bafcf1eacc8a77169b27f2253144e
EngelRyan <ryaneengel@gmail.com>
2024-08-01 11:39:22 -0300

    Solved

--- BEGINNER/Taxes/Main.java
+++ BEGINNER/Taxes/Main.java
@@ -11,17 +11,25 @@ public class Main {
 
         if (salary>4500) {
             taxes = ((salary-4500)*28)/100;
-            System.out.println(taxes);
             salary = salary-(salary-4500);
-            System.out.println(salary);
         }
-        else if(salary>3000 && salary <= 4500){
-            taxes = ((salary-3000)*18)/100;
-            System.out.println(taxes);
+        if(salary>3000 && salary <= 4500){
+            taxes += ((salary-3000)*18)/100;
             salary = salary-(salary-3000);
-            System.out.println(salary);
         }
-       
+        if(salary>2000 && salary <= 3000){
+            taxes += ((salary-2000)*8)/100;
+            salary = salary-(salary-2000);
+        }
+        
+        if(taxes>0){
+            System.out.printf("R$ %.2f",taxes);
+            System.out.println();
+        }
+        else{
+            System.out.println("Isento");
+        }
+        
         input.close();
     }
 }
