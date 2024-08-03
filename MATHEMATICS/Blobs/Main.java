de24dbbeed75082786469983cff9caa04f3f5d82
EngelRyan <ryaneengel@gmail.com>
2024-08-03 11:12:57 -0300

    New

---
+++ MATHEMATICS/Blobs/Main.java
@@ -0,0 +1,25 @@
+package MATHEMATICS.Blobs;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        
+        int[] howMany  = new int[n];
+
+        while(n --> 0 ){
+            double food = input.nextDouble();
+
+
+
+        }
+    }
+    
+}
