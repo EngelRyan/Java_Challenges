a1143b5224eff5c158ed196aae242b8317c94162
EngelRyan <ryaneengel@gmail.com>
2024-08-02 23:28:14 -0300

    Update

--- MATHEMATICS/Friends/Main.java
+++ MATHEMATICS/Friends/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    static private int[] values = new int[10];
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
 
