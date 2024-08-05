31c0a5be0631dfa170dec151eb2016aae491bf28
EngelRyan <ryaneengel@gmail.com>
2024-08-05 11:02:38 -0300

    Update

---
+++ BEGINNER/Animal/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner input = new Scanner(System.in);
+
+        String vertebrate = input.nextLine();
+        String typeClass = input.nextLine();
+        String food = input.nextLine();
+
+        classify(vertebrate,typeClass,food);
+
+        input.close();
+    }
+    
+ 
+}
