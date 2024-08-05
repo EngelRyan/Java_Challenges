9c12bbdff0dc35086a1830b61e235eb28fb1145f
EngelRyan <ryaneengel@gmail.com>
2024-08-05 11:12:52 -0300

    Update

--- BEGINNER/Animal/Main.java
+++ BEGINNER/Animal/Main.java
@@ -15,6 +15,47 @@ public class Main {
 
         input.close();
     }
-    
- 
+    public static void classify(String vertebrate, String typeClass, String food){
+
+        if (vertebrate.equals("vertebrado")) {
+
+            if(typeClass.equals("ave")){
+
+                if (food.equals("carnivoro")) {
+                    System.out.println("aguia");
+                }
+                else{
+                   System.out.println("pomba"); 
+                }
+            }
+            else{
+                if (food.equals("onivoro")) {
+                    System.out.println("homem");
+                }
+                else{
+                    System.out.println("vaca");
+                }
+            }
+        }
+        else{
+            if (typeClass.equals("inseto")) {
+
+                if (food.equals("hematofago")) {
+                    System.out.println("pulga");
+                }
+                else{
+                    System.out.println("lagarta");
+                }
+                
+            }
+            else{
+                if (food.equals("hematofago")) {
+                    System.out.println("sanguessuga");
+                }
+                else{
+                    System.out.println("minhoca");
+                }
+            }
+        }
+    }
 }
