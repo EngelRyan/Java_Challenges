bd85c2ef74603592fe38e3678816fe827762ee7a
EngelRyan <ryaneengel@gmail.com>
2024-08-06 15:06:02 -0300

    Update

--- BEGINNER/Event Time/Main.java
+++ BEGINNER/Event Time/Main.java
@@ -1,13 +1,50 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
- 
     public static void main(String[] args) throws IOException {
- 
         Scanner input = new Scanner(System.in);
 
-        
+        String startDay = input.nextLine();
+        String startTime = input.nextLine();
+        String endDay = input.nextLine();
+        String endTime = input.nextLine();
+
+        //Pegando o dia que começa
+        String[] splitTime = startDay.split(" ");
+        int startSeconds = Integer.parseInt(splitTime[1].trim())*86400;
+        //Pegando o tempo que começa
+        splitTime = startTime.split(":");
+        int t = 3600;
+        for(String time : splitTime){
+            startSeconds += Integer.parseInt(time.trim())*t;
+            t /= 60;
+        }
+
+        //Pegando o dia que termina
+        splitTime = endDay.split(" ");
+        int endSeconds = Integer.parseInt(splitTime[1].trim())*86400;
+        //Pegando o tempo que termina
+        splitTime = endTime.split(":");
+        t = 3600;
+        for(String time : splitTime){
+            endSeconds += Integer.parseInt(time.trim())*t;
+            t /= 60;
+        }
+
+        System.out.println(startSeconds);
+        System.out.println(endSeconds);
+
+
+
+
+
+
+
+
+
     }
  
 }
